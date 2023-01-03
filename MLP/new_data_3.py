390a7db1c62c90267d7e9d4db547cd39b5dc9cb8
Ivan Ezhov <ezhovivan@gmail.com>
2023-01-03 21:10:02 +0100

    first upload of the files

---
+++ MLP/new_data_3.py
@@ -0,0 +1,32 @@
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from torch.utils.data import random_split
+import torch
+from torch import Tensor
+
+import numpy as np
+import os
+
+class HELICoiD(Dataset):
+    #Constructor for initially loading
+    def __init__(self,path):
+        #df = read_csv(path, header=None)
+        #df = np.load(path + 'y_and_params/' + file)
+    
+        self.spectra_dir_path = path
+        self.dataset_size = len(os.listdir(self.spectra_dir_path))
+
+    def __len__(self):
+        return self.dataset_size
+
+    def __getitem__(self, idx):
+        filepath = os.path.join(self.spectra_dir_path, str(idx) + ".npz")
+        #print("idx: ", idx, " ,filepath: ", filepath)
+        npz_data = np.load(filepath) 
+        spect = npz_data['spectra'].astype('float32')
+        params = npz_data['params'].astype('float32')
+        #print(params.type)
+        #print(spect)
+        #print(params)
+        return spect, np.asarray([params[0],params[2],params[4]])
+
---
+++ MLP/new_train.py
@@ -0,0 +1,85 @@
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from torch.utils.data import random_split
+import torch
+from tqdm import tqdm
+
+from torch.nn import Module
+from torch.optim import Adam
+from torch.nn import L1Loss
+from torch.optim import lr_scheduler
+import time
+
+from new_data import HELICoiD
+from new_MLP import SpectraMLP
+
+def rmae(outputs, targets):
+    loss = torch.abs(outputs - targets) / torch.abs(targets)
+    loss = loss.mean(0) 
+    return loss
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+n_inputs = 301 
+n_outputs = 9
+epochs = 100
+lr = 0.01
+
+save_path='HELICoiD_best_model.pth'
+
+model = SpectraMLP(n_inputs, n_outputs)
+model.to(device)
+criterion = L1Loss()
+optimizer = Adam(model.parameters(), lr=lr)
+loss = 0.0
+
+path_to_data = "./synthetic_data/y_and_params/"
+HELICoiD_dataset = HELICoiD(path_to_data)
+
+train_size_percent = 0.9
+val_size_percent = 1 - train_size_percent
+datset_size= HELICoiD_dataset.__len__()
+train_set, val_set = random_split(HELICoiD_dataset, [round(datset_size*train_size_percent), round(datset_size*val_size_percent)])
+
+train_dl = DataLoader(train_set, batch_size=16448, shuffle=True)
+val_dl = DataLoader(val_set, batch_size=1024, shuffle=False)
+
+start = time.time()
+print(next(model.parameters()).is_cuda)
+for epoch in range(epochs):
+    print('Epoch {}/{}'.format(epoch+1, epochs))
+    print('-' * 10)
+    model.train()
+
+    # Iterate through training data loader
+    for i, (inputs, targets) in enumerate(tqdm(train_dl)):
+        inputs, targets = inputs.to(device), targets.to(device)
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        preds = outputs
+        
+        #rmae = torch.abs(outputs - targets) / torch.abs(targets)
+        #rmae = rmae.mean(0) 
+        loss = rmae(outputs, targets)  
+        print(loss)  
+
+        loss.sum().backward()
+        optimizer.step()
+    
+    val_loss = 0
+    model.eval()
+    for i, (inputs, targets) in enumerate(tqdm(val_dl)):
+        inputs, targets = inputs.to(device), targets.to(device)
+        outputs = model(inputs)
+        val_loss += rmae(outputs, targets)
+
+    print("validation loss at epoch {}:".format(epoch), val_loss/round(datset_size*val_size_percent))
+
+    torch.save(model, save_path)
+
+time_delta = time.time() - start
+print('Training complete in {:.0f}m {:.0f}s'.format(
+    time_delta // 60, time_delta % 60
+))
+
+    
